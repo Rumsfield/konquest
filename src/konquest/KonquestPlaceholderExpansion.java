59e345b38528a0eec8588751cd10cfe088aea7d1
Rumsfield <rrumsey92@gmail.com>
2021-07-13 20:38:24 -0700

    Added placeholder values.

--- src/konquest/KonquestPlaceholderExpansion.java
+++ src/konquest/KonquestPlaceholderExpansion.java
@@ -2,6 +2,12 @@ package konquest;
 
 import org.bukkit.entity.Player;
 
+import konquest.manager.KingdomManager;
+import konquest.manager.PlayerManager;
+import konquest.model.KonOfflinePlayer;
+import konquest.model.KonPlayer;
+import konquest.model.KonTerritoryType;
+import konquest.model.KonTown;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 /**
@@ -12,8 +18,13 @@ public class KonquestPlaceholderExpansion extends PlaceholderExpansion {
 
 	private KonquestPlugin plugin;
 	
+	private PlayerManager playerManager;
+	private KingdomManager kingdomManager;
+	
 	public KonquestPlaceholderExpansion(KonquestPlugin plugin) {
 		this.plugin = plugin;
+		playerManager = plugin.getKonquestInstance().getPlayerManager();
+		kingdomManager = plugin.getKonquestInstance().getKingdomManager();
 	}
 	
 	/**
@@ -96,20 +107,178 @@ public class KonquestPlaceholderExpansion extends PlaceholderExpansion {
         if(player == null){
             return "";
         }
-
-        // %someplugin_placeholder1%
-        if(identifier.equals("placeholder1")){
-            return plugin.getConfig().getString("placeholder1", "value doesnt exist");
+        
+        KonOfflinePlayer offlinePlayer;
+        KonPlayer onlinePlayer;
+        String list;
+        int count;
+        
+        // Provide placeholder value info
+        String result = null;
+        switch(identifier.toLowerCase()) {
+	        /* %konquest_kingdom% - player's kingdom name */
+        	case "kingdom":
+        		offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	result = offlinePlayer == null ? "" : offlinePlayer.getKingdom().getName();
+	        	break;
+	        /* %konquest_exile% - player's exile kingdom name */
+        	case "exile":
+        		offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	result = offlinePlayer == null ? "" : offlinePlayer.getExileKingdom().getName();
+	        	break;
+	        /* %konquest_barbarian% - true if player is barbarian, else false */
+	        case "barbarian":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	result = offlinePlayer == null ? "" : String.valueOf(offlinePlayer.isBarbarian());
+	        	break;
+	        /* %konquest_towns_lord% - comma-separated list of player's lord only towns */
+	        case "towns_lord":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	list = "";
+	        	if(offlinePlayer != null) {
+		    		for(KonTown town : offlinePlayer.getKingdom().getTowns()) {
+		    			if(town.isPlayerLord(offlinePlayer.getOfflineBukkitPlayer())) {
+		    				list = list + town.getName() + ",";
+		    			}
+		    		}
+		    		if(list.length() > 1) {
+		    			list = list.substring(0,list.length()-1);
+	    			}
+	        	}
+	    		result = list;
+	        	break;
+	        /* %konquest_towns_knight% - comma-separated list of player's knight only towns */
+	        case "towns_knight":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	list = "";
+	        	if(offlinePlayer != null) {
+		    		for(KonTown town : offlinePlayer.getKingdom().getTowns()) {
+		    			if(town.isPlayerElite(offlinePlayer.getOfflineBukkitPlayer()) &&
+		    					!town.isPlayerLord(offlinePlayer.getOfflineBukkitPlayer())) {
+		    				list = list + town.getName() + ",";
+		    			}
+		    		}
+		    		if(list.length() > 1) {
+		    			list = list.substring(0,list.length()-1);
+	    			}
+	        	}
+	    		result = list;
+	        	break;
+	        /* %konquest_towns_resident% - comma-separated list of player's resident only towns */
+	        case "towns_resident":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	list = "";
+	        	if(offlinePlayer != null) {
+		    		for(KonTown town : offlinePlayer.getKingdom().getTowns()) {
+		    			if(town.isPlayerResident(offlinePlayer.getOfflineBukkitPlayer()) &&
+		    					!town.isPlayerElite(offlinePlayer.getOfflineBukkitPlayer())) {
+		    				list = list + town.getName() + ",";
+		    			}
+		    		}
+		    		if(list.length() > 1) {
+		    			list = list.substring(0,list.length()-1);
+	    			}
+	        	}
+	    		result = list;
+	        	break;
+	        /* %konquest_towns_all% - comma-separated list of player's all towns */
+	        case "towns_all":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	list = "";
+	        	if(offlinePlayer != null) {
+		    		for(KonTown town : offlinePlayer.getKingdom().getTowns()) {
+		    			if(town.isPlayerResident(offlinePlayer.getOfflineBukkitPlayer())) {
+		    				list = list + town.getName() + ",";
+		    			}
+		    		}
+		    		if(list.length() > 1) {
+		    			list = list.substring(0,list.length()-1);
+	    			}
+	        	}
+	    		result = list;
+	        	break;
+	        /* %konquest_territory% - player's current location territory type */
+	        case "territory":
+	        	result = "";
+	        	onlinePlayer = playerManager.getPlayer(player);
+	        	if(onlinePlayer != null && player.isOnline()) {
+		        	if(kingdomManager.isChunkClaimed(player.getLocation())) {
+		        		result = kingdomManager.getChunkTerritory(player.getLocation()).getTerritoryType().name();
+		        	} else {
+		        		result = KonTerritoryType.WILD.name();
+		        	}
+	        	}
+	        	break;
+	        /* %konquest_land% - player's current location territory name */
+	        case "land":
+	        	result = "";
+	        	onlinePlayer = playerManager.getPlayer(player);
+	        	if(onlinePlayer != null && player.isOnline()) {
+		        	if(kingdomManager.isChunkClaimed(player.getLocation())) {
+		        		result = kingdomManager.getChunkTerritory(player.getLocation()).getName();
+		        	} else {
+		        		result = KonTerritoryType.WILD.name();
+		        	}
+	        	}
+	        	break;
+	        /* %konquest_claimed% - true if the player's current location is claimed, else false */
+	        case "claimed":
+	        	result = "";
+	        	onlinePlayer = playerManager.getPlayer(player);
+	        	if(onlinePlayer != null && player.isOnline()) {
+	        		result = String.valueOf(kingdomManager.isChunkClaimed(player.getLocation()));
+	        	}
+	        	break;
+	        /* %konquest_score% - player's score value */
+	        case "score":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	result = offlinePlayer == null ? "" : String.valueOf(kingdomManager.getPlayerScore(offlinePlayer));
+	        	break;
+	        /* %konquest_prefix% - player's prefix title */
+	        case "prefix":
+	        	result = "not yet implemented";
+	        	break;
+	        /* %konquest_lordships% - number of player's lordships */
+	        case "lordships":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	count = 0;
+	        	if(offlinePlayer != null) {
+		    		for(KonTown town : offlinePlayer.getKingdom().getTowns()) {
+		    			if(town.isPlayerLord(offlinePlayer.getOfflineBukkitPlayer())) {
+		    				count++;
+		    			}
+		    		}
+	        	}
+	        	result = String.valueOf(count);
+	        	break;
+	        /* %konquest_residencies% - number of player's total residencies, including lordships */
+	        case "residencies":
+	        	offlinePlayer = playerManager.getOfflinePlayer(player);
+	        	count = 0;
+	        	if(offlinePlayer != null) {
+		    		for(KonTown town : offlinePlayer.getKingdom().getTowns()) {
+		    			if(town.isPlayerResident(offlinePlayer.getOfflineBukkitPlayer())) {
+		    				count++;
+		    			}
+		    		}
+	        	}
+	        	result = String.valueOf(count);
+	        	break;
+	        /* %konquest_chat% - true if player is using global chat, else false */
+	        case "chat":
+	        	onlinePlayer = playerManager.getPlayer(player);
+	        	result = onlinePlayer == null ? "" : String.valueOf(onlinePlayer.isGlobalChat());
+	        	break;
+	        /* %konquest_combat% - true if player is combat tagged, else false */
+	        case "combat":
+	        	onlinePlayer = playerManager.getPlayer(player);
+	        	result = onlinePlayer == null ? "" : String.valueOf(onlinePlayer.isCombatTagged());
+	        	break;
+	        default: 
+	        	break;
         }
 
-        // %someplugin_placeholder2%
-        if(identifier.equals("placeholder2")){
-            return plugin.getConfig().getString("placeholder2", "value doesnt exist");
-        }
- 
-        // We return null if an invalid placeholder (f.e. %someplugin_placeholder3%) 
-        // was provided
-        return null;
+        return result;
     }
 
 }
