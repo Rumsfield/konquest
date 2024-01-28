4b34aeb351dd3ddb1098ffd10c429bb1fec570ec
TropicalShadow <ddlogger123@gmail.com>
2024-01-28 23:23:59 +0000

    fix: resolve the correct Dynmap plugin from local

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies{
     compileOnly("com.comphenix.protocol:ProtocolLib:5.0.0")
     compileOnly("org.maxgamer:QuickShop:5.1.2.2-SNAPSHOT")
     compileOnly("com.acrobot.chestshop:chestshop:3.12")
-    compileOnly("org.dynmap:dynmap:3.4-beta-2-spigot")
+    compileOnly("org.dynmap:Dynmap:3.4-beta-2-spigot")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("com.discordsrv:discordsrv:1.27.0")
     compileOnly("me.clip:placeholderapi:2.11.2")
