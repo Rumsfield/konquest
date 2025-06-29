e7db0f4c44fa0084fb400285cbd782b12aad209b
Rumsfield <rrumsey92@gmail.com>
2025-06-28 20:32:22 -0700

    Feature/329 special property flag (#339)
    * Added specialize property flag #329
    
    * Updated all language file messages #329

--- core/src/main/java/com/github/rumsfield/konquest/model/KonPropertyFlag.java
+++ core/src/main/java/com/github/rumsfield/konquest/model/KonPropertyFlag.java
@@ -10,36 +10,37 @@ public enum KonPropertyFlag {
 	// Descriptions come from MessagePath so that they can be translated.
 
 	// Properties for territories
-	TRAVEL			(MessagePath.PROPERTIES_TRAVEL_NAME.getMessage(), 	MessagePath.PROPERTIES_TRAVEL.getMessage()),
-	PVP				(MessagePath.PROPERTIES_PVP_NAME.getMessage(), 		MessagePath.PROPERTIES_PVP.getMessage()),
-	PVE				(MessagePath.PROPERTIES_PVE_NAME.getMessage(), 		MessagePath.PROPERTIES_PVE.getMessage()),
-	BUILD			(MessagePath.PROPERTIES_BUILD_NAME.getMessage(), 	MessagePath.PROPERTIES_BUILD.getMessage()),
-	USE				(MessagePath.PROPERTIES_USE_NAME.getMessage(), 		MessagePath.PROPERTIES_USE.getMessage()),
-	CHEST			(MessagePath.PROPERTIES_CHEST_NAME.getMessage(), 	MessagePath.PROPERTIES_CHEST.getMessage()),
-	MOBS			(MessagePath.PROPERTIES_MOBS_NAME.getMessage(), 	MessagePath.PROPERTIES_MOBS.getMessage()),
-	PORTALS			(MessagePath.PROPERTIES_PORTALS_NAME.getMessage(), 	MessagePath.PROPERTIES_PORTALS.getMessage()),
-	ENTER			(MessagePath.PROPERTIES_ENTER_NAME.getMessage(), 	MessagePath.PROPERTIES_ENTER.getMessage()),
-	EXIT			(MessagePath.PROPERTIES_EXIT_NAME.getMessage(), 	MessagePath.PROPERTIES_EXIT.getMessage()),
-	SHOP			(MessagePath.PROPERTIES_SHOP_NAME.getMessage(), 	MessagePath.PROPERTIES_SHOP.getMessage()),
-	ARENA		    (MessagePath.PROPERTIES_ARENA_NAME.getMessage(), 	MessagePath.PROPERTIES_ARENA.getMessage()),
+	TRAVEL			(MessagePath.PROPERTIES_TRAVEL_NAME.getMessage(), 		MessagePath.PROPERTIES_TRAVEL.getMessage()),
+	PVP				(MessagePath.PROPERTIES_PVP_NAME.getMessage(), 			MessagePath.PROPERTIES_PVP.getMessage()),
+	PVE				(MessagePath.PROPERTIES_PVE_NAME.getMessage(), 			MessagePath.PROPERTIES_PVE.getMessage()),
+	BUILD			(MessagePath.PROPERTIES_BUILD_NAME.getMessage(), 		MessagePath.PROPERTIES_BUILD.getMessage()),
+	USE				(MessagePath.PROPERTIES_USE_NAME.getMessage(), 			MessagePath.PROPERTIES_USE.getMessage()),
+	CHEST			(MessagePath.PROPERTIES_CHEST_NAME.getMessage(), 		MessagePath.PROPERTIES_CHEST.getMessage()),
+	MOBS			(MessagePath.PROPERTIES_MOBS_NAME.getMessage(), 		MessagePath.PROPERTIES_MOBS.getMessage()),
+	PORTALS			(MessagePath.PROPERTIES_PORTALS_NAME.getMessage(), 		MessagePath.PROPERTIES_PORTALS.getMessage()),
+	ENTER			(MessagePath.PROPERTIES_ENTER_NAME.getMessage(), 		MessagePath.PROPERTIES_ENTER.getMessage()),
+	EXIT			(MessagePath.PROPERTIES_EXIT_NAME.getMessage(), 		MessagePath.PROPERTIES_EXIT.getMessage()),
+	SHOP			(MessagePath.PROPERTIES_SHOP_NAME.getMessage(), 		MessagePath.PROPERTIES_SHOP.getMessage()),
+	ARENA		    (MessagePath.PROPERTIES_ARENA_NAME.getMessage(), 		MessagePath.PROPERTIES_ARENA.getMessage()),
 
 	// Properties specifically for towns/capitals
-	CAPTURE			(MessagePath.PROPERTIES_CAPTURE_NAME.getMessage(), 	MessagePath.PROPERTIES_CAPTURE.getMessage()),
-	CLAIM			(MessagePath.PROPERTIES_CLAIM_NAME.getMessage(), 	MessagePath.PROPERTIES_CLAIM.getMessage()),
-	UNCLAIM			(MessagePath.PROPERTIES_UNCLAIM_NAME.getMessage(), 	MessagePath.PROPERTIES_UNCLAIM.getMessage()),
-	UPGRADE			(MessagePath.PROPERTIES_UPGRADE_NAME.getMessage(), 	MessagePath.PROPERTIES_UPGRADE.getMessage()),
-	PLOTS			(MessagePath.PROPERTIES_PLOTS_NAME.getMessage(), 	MessagePath.PROPERTIES_PLOTS.getMessage()),
-	
+	CAPTURE			(MessagePath.PROPERTIES_CAPTURE_NAME.getMessage(), 		MessagePath.PROPERTIES_CAPTURE.getMessage()),
+	CLAIM			(MessagePath.PROPERTIES_CLAIM_NAME.getMessage(), 		MessagePath.PROPERTIES_CLAIM.getMessage()),
+	UNCLAIM			(MessagePath.PROPERTIES_UNCLAIM_NAME.getMessage(), 		MessagePath.PROPERTIES_UNCLAIM.getMessage()),
+	UPGRADE			(MessagePath.PROPERTIES_UPGRADE_NAME.getMessage(), 		MessagePath.PROPERTIES_UPGRADE.getMessage()),
+	PLOTS			(MessagePath.PROPERTIES_PLOTS_NAME.getMessage(), 		MessagePath.PROPERTIES_PLOTS.getMessage()),
+	SPECIALIZE		(MessagePath.PROPERTIES_SPECIALIZE_NAME.getMessage(),	MessagePath.PROPERTIES_SPECIALIZE.getMessage()),
+
 	// Properties for kingdoms
-	PEACEFUL		(MessagePath.PROPERTIES_PEACEFUL_NAME.getMessage(), MessagePath.PROPERTIES_PEACEFUL.getMessage()),
-	GOLEMS			(MessagePath.PROPERTIES_GOLEMS_NAME.getMessage(), 	MessagePath.PROPERTIES_GOLEMS.getMessage()),
+	PEACEFUL		(MessagePath.PROPERTIES_PEACEFUL_NAME.getMessage(),	 	MessagePath.PROPERTIES_PEACEFUL.getMessage()),
+	GOLEMS			(MessagePath.PROPERTIES_GOLEMS_NAME.getMessage(), 		MessagePath.PROPERTIES_GOLEMS.getMessage()),
 
 	// Properties for membership
-	JOIN			(MessagePath.PROPERTIES_JOIN_NAME.getMessage(), 	MessagePath.PROPERTIES_JOIN.getMessage()),
-	LEAVE			(MessagePath.PROPERTIES_LEAVE_NAME.getMessage(), 	MessagePath.PROPERTIES_LEAVE.getMessage()),
-	PROMOTE			(MessagePath.PROPERTIES_PROMOTE_NAME.getMessage(), 	MessagePath.PROPERTIES_PROMOTE.getMessage()),
-	DEMOTE			(MessagePath.PROPERTIES_DEMOTE_NAME.getMessage(), 	MessagePath.PROPERTIES_DEMOTE.getMessage()),
-	TRANSFER		(MessagePath.PROPERTIES_TRANSFER_NAME.getMessage(), MessagePath.PROPERTIES_TRANSFER.getMessage()),
+	JOIN			(MessagePath.PROPERTIES_JOIN_NAME.getMessage(), 		MessagePath.PROPERTIES_JOIN.getMessage()),
+	LEAVE			(MessagePath.PROPERTIES_LEAVE_NAME.getMessage(), 		MessagePath.PROPERTIES_LEAVE.getMessage()),
+	PROMOTE			(MessagePath.PROPERTIES_PROMOTE_NAME.getMessage(), 		MessagePath.PROPERTIES_PROMOTE.getMessage()),
+	DEMOTE			(MessagePath.PROPERTIES_DEMOTE_NAME.getMessage(), 		MessagePath.PROPERTIES_DEMOTE.getMessage()),
+	TRANSFER		(MessagePath.PROPERTIES_TRANSFER_NAME.getMessage(), 	MessagePath.PROPERTIES_TRANSFER.getMessage()),
 
 	NONE			("N/A", 		"Nothing");
 	
