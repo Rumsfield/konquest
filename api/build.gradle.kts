0f761d58d6cdd0aabc068cb01df0a4db50e56b86
Rumsfield <rrumsey92@gmail.com>
2025-07-20 15:48:56 -0700

    Updated Spigot API and support for 1.21.8 #317

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies{
-    compileOnly("org.spigotmc:spigot-api:1.21.7-R0.1-SNAPSHOT") // Primary API
+    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT") // Primary API
 }
 
 tasks {
